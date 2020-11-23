6604bd8bd1103d0394ee103576780621eca0d51d
IShungite <ishungite@gmail.com>
2020-11-23 12:45:02 +0100

    add dropdown for contract

--- indeed/src/Form/ContractType.php
+++ indeed/src/Form/ContractType.php
@@ -6,14 +6,21 @@ use App\Entity\Contract;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class ContractType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name', ChoiceType::class, [
+                'choices'  => [
+                    'CDD' => 'CDD',
+                    'CDI' => 'CDI',
+                    'FREE' => 'FREE',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
--- indeed/src/Form/ContractTypeType.php
+++ indeed/src/Form/ContractTypeType.php
@@ -6,14 +6,20 @@ use App\Entity\ContractType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
 
 class ContractTypeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('name')
-        ;
+            ->add('name', ChoiceType::class, [
+                'choices'  => [
+                    'Temps plein' => 'Temps plein',
+                    'Temps partiel' => 'Temps partiel',
+                ],
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver)
