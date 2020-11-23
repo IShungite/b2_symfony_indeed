71b00c12d2599dd5e177e5e72b0aceb12c81b395
laurent.marechal@ynov.com <laurent.marechal@ynov.com>
2020-11-23 12:07:11 +0100

    makeOffer page done

--- indeed/src/Controller/HomeController.php
+++ indeed/src/Controller/HomeController.php
@@ -5,7 +5,10 @@ namespace App\Controller;
 use App\ContractTypeFixtures\ContractTypeFixtures;
 use App\Entity\Offer;
 use App\Repository\OfferRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -29,6 +32,40 @@ class HomeController extends AbstractController
     {
         return $this->render('home/offer.html.twig', [
             'offer' => $offer
+        ]);
+    }
+
+    /**
+     * @Route("/makeOffer", name="makeOffer")
+     */
+    public function makeOffer(Request $request, EntityManagerInterface $em)
+    {
+        $offre = new Offer();
+        $form = $this->createFormBuilder($offre)
+            ->add('title')
+            ->add('description')
+            ->add('adresse')
+            ->add('postal_code')
+            ->add('city')
+            ->add('contract_end')
+            ->add('contract')
+            ->add('contract_type')
+            ->add('submit', SubmitType::class)
+            ->getForm();
+
+        $offre->setCreationDate(new \DateTime());
+        $offre->setUpdateDate(new \DateTime());
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($offre);
+            $em->flush();
+        }
+
+
+        return $this->render('home/makeOffer.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
